b77b693fe267953b6c6405b4af04ed696815b897
ustcck <ustcck@outlook.com>
2016-07-18 15:19:01 +0800

    haha

--- test2/EventUtils.js
+++ test2/EventUtils.js
@@ -21,5 +21,27 @@ var EventUtil = {
             element['on' + type] = null;
         }
 
+    },
+    getEvent:function (event) {
+        return event ? event : window.event;
+
+    },
+    getTarget:function (event) {
+        return event.target || event.srcElement;
+    },
+
+    preventDefault:function (event) {
+        if (event.preventDefault){
+            event.preventDefault();
+        }else{
+            event.returnValue = false;
+        }
+    },
+    stopPropagation:function (event) {
+        if(event.stopPropagation){
+            event.stopPropagation();
+        }else {
+            event.cancelBubble = true;
+        }
     }
 };
